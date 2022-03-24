9ec9526c228bf23d3f644559027c82c6744b4b20
Carlos Sibalatani <carlos.sibalatani@mhss.gov.na>
2022-03-24 23:17:52 +0200

    update merge function

--- src/utils/data-merge.ts
+++ src/utils/data-merge.ts
@@ -4,8 +4,8 @@ function arrayUnion(array1: any[], array2: any[]) {
   array2.forEach((item: { name: string }) =>
     map.set(item.name, { ...map.get(item.name), ...item })
   );
-  const mergedArr = Array.from(map.values());
-  return mergedArr.map((element, index) => ({
+  let mergedArr = Array.from(map.values());
+  mergedArr = mergedArr.map((element, index) => ({
     name: element.name,
     key: index,
     values: Object.keys(element)
@@ -15,6 +15,8 @@ function arrayUnion(array1: any[], array2: any[]) {
         value: element[v],
       })),
   }));
+
+  return mergedArr.filter((element) => element.values.length > 0);
 }
 
 export default arrayUnion;
